e7d33dc50548634119aa0c3f84c8503d07791aa3
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-07-18 09:36:58 +0530

    Refactor guessing logic to improve attempt handling and game over conditions

--- app.js
+++ app.js
@@ -2,29 +2,31 @@ let num = Math.random() * 10 + 1;
 randomNum = Math.floor(num);
 console.log(randomNum);
 
+let attempt = 5;
+
 
 function clickonGuessActionbtn() {
     let guessNumber = document.getElementById("guessNumber");
 
-    let attempt = 5;
 
-    if (attempt == 0) {
 
+    for (let i = attempt; i >= 0; i--) {
+
+        if (randomNum > guessNumber.value) {
+            console.log("Guessed number is less than the actual Number you have");
+        } else if (randomNum < guessNumber.value) {
+            console.log("Guessed number is higher than the actual Number you have");
+
+        } else if(randomNum == guessNumber.value) {
+            console.log("Congratulations you have guessed the correct number");
+
+        }else{
         console.log("Game over buddy");
 
-    }
+        }
 
-    if (randomNum > guessNumber.value) {
-        console.log("Guessed number is less than the actual Number you have");
-    } else if (randomNum < guessNumber.value) {
-        console.log("Guessed number is higher than the actual Number you have");
-
-    } else {
-        console.log("Congratulations you have guessed the correct number");
+        console.log("attempts left" + attempt);
 
     }
-
-    attempt--;
-
 
 }
