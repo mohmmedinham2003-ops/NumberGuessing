3125fa61a6fdabf2603f40ada2e46b59532586d9
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-07-18 09:30:35 +0530

    Implement number guessing logic and update HTML structure

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("hola");
+let num = Math.random() * 10 + 1;
+randomNum = Math.floor(num);
+console.log(randomNum);
+
+
+function clickonGuessActionbtn() {
+    let guessNumber = document.getElementById("guessNumber");
+
+    let attempt = 5;
+
+    if (attempt == 0) {
+
+        console.log("Game over buddy");
+
+    }
+
+    if (randomNum > guessNumber.value) {
+        console.log("Guessed number is less than the actual Number you have");
+    } else if (randomNum < guessNumber.value) {
+        console.log("Guessed number is higher than the actual Number you have");
+
+    } else {
+        console.log("Congratulations you have guessed the correct number");
+
+    }
+
+    attempt--;
+
+
+}
