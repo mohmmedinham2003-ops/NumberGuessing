061ccfb9ff11152102a73a71da0c3ad783758f4b
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-07-18 10:24:02 +0530

    Reset attempt counter and enhance game feedback for user guesses

--- app.js
+++ app.js
@@ -2,37 +2,42 @@ let num = Math.random() * 10 + 1;
 randomNum = Math.floor(num);
 console.log(randomNum);
 
-let attempt = 3;
-
-
+let attempt = 0;
+const maxAttemp = 3;
 function clickonGuessActionbtn() {
     let guessNumber = document.getElementById("guessNumber");
 
     let textInput = guessNumber.value;
     heading = document.getElementById("heading");
-    
-    if (randomNum > textInput) {
-        heading.innerHTML = "Guessed number is less than the actual Number you have";
-        //console.log("Guessed number is less than the actual Number you have");
+    if (attempt < maxAttemp) {
+        attempt++;
+        if (randomNum > textInput) {
+            heading.innerHTML = "Guessed number is less than the actual Number you have";
+            //console.log("Guessed number is less than the actual Number you have");
 
-    } else if (randomNum < textInput) {
-        heading.innerHTML = "Guessed number is higher than the actual Number you have"
-        //console.log("Guessed number is higher than the actual Number you have");
+        } else if (randomNum < textInput) {
+            heading.innerHTML = "Guessed number is higher than the actual Number you have"
+            //console.log("Guessed number is higher than the actual Number you have");
 
-    } else if (randomNum == textInput) {
-        heading.innerHTML = "Congratulations you have guessed the correct number";
-        //console.log("Congratulations you have guessed the correct number");
-        Swal.fire({
-        title: "Good job!",
-        text: "You won bro!",
-        icon: "success"
-    });
+        } else {
+            heading.innerHTML = "Congratulations you have guessed the correct number";
+            //console.log("Congratulations you have guessed the correct number");
+            Swal.fire({
+                title: "Good job!",
+                text: "You won bro!",
+                icon: "success"
+            });
 
+        }
     } else {
         heading.innerHTML = "Game over buddy";
+        Swal.fire({
+            icon: "error",
+            title: "Oops... attemps are over",
+            footer: "<a href=\"#\">Why do I have this issue?</a>"
+        });
 
     }
 
-    
 
 }
